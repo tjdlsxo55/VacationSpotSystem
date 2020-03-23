70925224906f1e602fe00a5abc7e9705ecb63d8c
SW09 <SW09@172.18.57.111>
2020-03-23 16:27:19 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+ public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Select one number between 1 - 6 !");
+		System.out.println("1.Price of Vacation Spot");
+		System.out.println("2.Size of Vacation Spot");
+		System.out.println("3.Local Situation of Vacation Spot");
+		System.out.println("4.Food of Vacation Spot");
+		System.out.println("5.Number of Users");
+		System.out.println("6.Log Out");
+		}
+}
